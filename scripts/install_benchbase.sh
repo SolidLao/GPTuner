627081baa4a14da01067a3594a22bc1664a4d41e
Wangyibo321 <126743347+Wangyibo321@users.noreply.github.com>
2024-03-18 14:42:38 +0800

    Update install_benchbase.sh
    mkdir and java-21

--- scripts/install_benchbase.sh
+++ scripts/install_benchbase.sh
@@ -1,5 +1,10 @@
+mkdir ../optimization_results/
+mkdir ../optimization_results/temp_results/
+mkdir ../optimization_results/$1/
+mkdir ../optimization_results/$1/log/
 sudo apt-get update
 sudo apt-get install git
+sudo apt install openjdk-21-jdk
 git clone --depth 1 https://github.com/cmu-db/benchbase.git ../benchbase
 cd ../benchbase
 ./mvnw clean package -P $1
