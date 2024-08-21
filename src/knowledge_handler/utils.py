9dc3d2329403a5fd33a61f53e5ffe135b40ade80
Yibo Wang <126743347+Wangyibo321@users.noreply.github.com>
2024-08-21 15:59:19 +0800

    Update utils.py

--- src/knowledge_handler/utils.py
+++ src/knowledge_handler/utils.py
@@ -11,16 +11,15 @@ def get_hardware_info():
     total_disk_space = total_disk_space / (1024 * 1024 * 1024)
     return available_cpu_cores, int(total_memory), int(total_disk_space)
 
-def get_disk_type(device="sda"):
-    rotational_path = f'/sys/block/{device}/queue/rotational'
-    if os.path.exists(rotational_path):
-        with open(rotational_path, 'r') as file:
-            rotational_value = file.read().strip()
-            if rotational_value == '0':
-                return 'SSD'
-            elif rotational_value == '1':
-                return 'HDD'
-            else:
-                return 'Unknown'
-    else:
-        return 'Unknown'
+def get_disk_type(devices=["sda", "nvme0n1", "vda", "hda"]):
+    for device in devices:
+        rotational_path = f'/sys/block/{device}/queue/rotational'
+        if os.path.exists(rotational_path):
+            with open(rotational_path, 'r') as file:
+                rotational_value = file.read().strip()
+                if rotational_value == '0':
+                    return 'SSD'
+                elif rotational_value == '1':
+                    return 'HDD'
+
+    return 'Unknown'
